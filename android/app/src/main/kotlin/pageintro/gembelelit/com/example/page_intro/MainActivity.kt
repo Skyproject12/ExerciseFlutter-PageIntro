752f54f33e155865bd08d46a60b0eee28cb9a8e4
Kadek Sumendra <sumendrakadek108@gmail.com>
2020-01-14 11:07:56 +0700

    First Commit

---
+++ android/app/src/main/kotlin/pageintro/gembelelit/com/example/page_intro/MainActivity.kt
@@ -0,0 +1,12 @@
+package pageintro.gembelelit.com.example.page_intro
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
